1a55d17741278d986643bb4f8832bb792857bfd0
abrahamalanya <alanyaoficial@gmail.com>
2026-04-07 03:42:44 -0500

    update route login

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome'); // Eliminar blade
+    return redirect()->route('login');
 });
 
 Route::get('/documentation', function () {
